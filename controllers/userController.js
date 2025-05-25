b18ff9cfd81ced4a8e7ce502d126feeb3c63b456
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-05-25 17:37:24 +0500

    add JWT authentication to login response

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userSchema.js';
 import validator from 'validator'
 import bcrypt from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
 
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -41,3 +42,40 @@ export const registerUser = async (req, res) => {
   }
 
 }
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Please provide both email and password' });
+  }
+
+  try {
+    
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+     
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      message: 'Login successful',
+       token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
