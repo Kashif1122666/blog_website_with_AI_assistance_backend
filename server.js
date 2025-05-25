be6357cabe37be321f0925f9bd3783ddbeaf1439
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-05-25 15:48:13 +0500

    add basic setup for express server

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello I am backend !');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
